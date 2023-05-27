d1e30f9e79e31cf0eeaca0fa19f89c3183fd7334
Reid Hansell <reidhansell@gmail.com>
2023-05-26 21:23:43 -0600

    add linux args to puppeteer

--- objects/Export.js
+++ objects/Export.js
@@ -107,7 +107,7 @@ class Export {
         fs.writeFileSync('table.html', html);
 
         // Launch Puppeteer, go to the page
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
         const page = await browser.newPage();
         await page.goto('file://' + path.resolve('table.html'));
 
